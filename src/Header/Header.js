bdd9006753ef0cff9ab4c24cc2206c058568b700
lichdu29 <dunguyen643@gmail.com>
2022-04-20 23:41:02 +0700

    update header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,11 +1,17 @@
-import "./Header.css"
+import "./Header.css";
 import Navbar from "./Navbar";
 import React from "react";
+import {ReactComponent as Group17} from '../media/Group17.svg'
+import {ReactComponent as Group171} from '../media/group17-1.svg'
 
 const Header = () => {
-  return <div className="header">
-    <Navbar/>
-  </div>
-}
+  return (
+    <div className="header">
+        <Navbar />
+        <Group17 className="background1"/>
+        <Group171 className="background2"/>
+    </div>
+  );
+};
 
 export default Header;
--- src/Header/Hero.js
+++ src/Header/Hero.js
@@ -1,9 +1,16 @@
 import React from "react";
+import {ReactComponent as Akash} from '../media/Akash.svg'
+import {ReactComponent as Desmos} from '../media/Desmos.svg'
+import {ReactComponent as Dig} from '../media/Dig.svg'
+import {ReactComponent as Emoney} from '../media/E-money.svg'
+import {ReactComponent as Atom} from '../media/Atom.svg'
+import {ReactComponent as Lum} from '../media/Lum.svg'
+import {ReactComponent as Akash} from '../media/Akash.svg'
 
 const Hero = () => {
   return (
-    <div>
-      
+    <div className="hero">
+
     </div>
   )
 }
