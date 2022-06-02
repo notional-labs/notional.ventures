f2759e5f03b8bde71430db13b4191ecd672158f2
Du Nguyen <dunguyen643@gmail.com>
2022-06-02 16:28:54 +0700

    fix UI

--- src/Body/Contribute/PartnerCarousel.js
+++ src/Body/Contribute/PartnerCarousel.js
@@ -2,9 +2,11 @@ import React from "react";
 import Osmosis from "../../media/logo/osmosis.png";
 import Juno from "../../media/logo/juno.png";
 import Cosmos from "../../media/logo/cosmos.png";
-import Stargaze from "../../media/logo/stargaze.png";
-import Cheq from "../../media/logo/cheqd.png";
+// import Stargaze from "../../media/logo/stargaze.png";
+// import Cheq from "../../media/logo/cheqd.png";
 import Omniflix from "../../media/logo/omniflix.png";
+import Sifchain from "../../media/logo/sifchain.png";
+import Evmos from "../../media/logo/evmos.png";
 import "./PartnerCarousel.css";
 
 const AutoSlide = () => {
@@ -22,28 +24,10 @@ const AutoSlide = () => {
             <img src={Juno} alt="" />
           </div>
           <div className="slide">
-            <img src={Stargaze} alt="" />
+            <img src={Sifchain} alt="" />
           </div>
           <div className="slide">
-            <img src={Cheq} alt="" />
-          </div>
-          <div className="slide">
-            <img src={Omniflix} alt="" />
-          </div>
-          <div className="slide">
-            <img src={Osmosis} alt="" />
-          </div>
-          <div className="slide">
-            <img src={Cosmos} alt="" />
-          </div>
-          <div className="slide">
-            <img src={Juno} alt="" />
-          </div>
-          <div className="slide">
-            <img src={Stargaze} alt="" />
-          </div>
-          <div className="slide">
-            <img src={Cheq} alt="" />
+            <img src={Evmos} alt="" />
           </div>
           <div className="slide">
             <img src={Omniflix} alt="" />
@@ -58,10 +42,28 @@ const AutoSlide = () => {
             <img src={Juno} alt="" />
           </div>
           <div className="slide">
-            <img src={Stargaze} alt="" />
+            <img src={Sifchain} alt="" />
           </div>
           <div className="slide">
-            <img src={Cheq} alt="" />
+            <img src={Evmos} alt="" />
+          </div>
+          <div className="slide">
+            <img src={Omniflix} alt="" />
+          </div>
+          <div className="slide">
+            <img src={Osmosis} alt="" />
+          </div>
+          <div className="slide">
+            <img src={Cosmos} alt="" />
+          </div>
+          <div className="slide">
+            <img src={Juno} alt="" />
+          </div>
+          <div className="slide">
+            <img src={Sifchain} alt="" />
+          </div>
+          <div className="slide">
+            <img src={Evmos} alt="" />
           </div>
           <div className="slide">
             <img src={Omniflix} alt="" />
