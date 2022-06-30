62fd7cc9e84043712ac5df08a499c978cec2e63a
Du Nguyen <dunguyen643@gmail.com>
2022-07-01 00:58:27 +0700

    add preloader

--- src/App.js
+++ src/App.js
@@ -7,21 +7,33 @@ import Project from "./Body/Project/Project";
 import Team from "./Body/Team/Team";
 import ScrollButton from "./Components/ScrollTopButton";
 import Contact from "./Body/Contact/Contact";
-import Footer from "./Footer/Footer"
+import Footer from "./Footer/Footer";
+import RingLoader from "react-spinners/RingLoader";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false)
+  useEffect (() => {
+    setIsLoading(true)
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 4000)
+  },[])
   return (
     <React.Fragment>
-      <div className = "root">
+      {isLoading && <div className="loader-container">
+        <RingLoader color="#eaebed" size={130}/>
+      </div>}
+      {!isLoading && <div className="root" >
         <Header />
         <Contributions />
         <Stake />
         <Project />
         <Team />
-        <Contact/>
-        <ScrollButton/>
-        <Footer/>
-      </div>
+        <Contact />
+        <ScrollButton />
+        <Footer />
+      </div>}
     </React.Fragment>
   );
 }
