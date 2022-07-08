d0bd6a39d6b2e4a78217cfc5682551e699c458ed
Master Pi <minh.vln140501@gmail.com>
2022-07-08 17:20:51 +0700

    hover

--- src/Body/Infrastructure/components/Graph.js
+++ src/Body/Infrastructure/components/Graph.js
@@ -8,51 +8,51 @@ import { useState, useCallback } from "react";
 const Graph = () => {
  
     const NODE_R = 8;
-    // const [highlightNodes, setHighlightNodes] = useState(new Set());
-    // const [highlightLinks, setHighlightLinks] = useState(new Set());
-    // const [hoverNode, setHoverNode] = useState(null);
+    const [highlightNodes, setHighlightNodes] = useState(new Set());
+    const [highlightLinks, setHighlightLinks] = useState(new Set());
+    const [hoverNode, setHoverNode] = useState(null);
 
-    // const updateHighlight = () => {
-    //     setHighlightNodes(highlightNodes);
-    //     setHighlightLinks(highlightLinks);
-    // };
+    const updateHighlight = () => {
+        setHighlightNodes(highlightNodes);
+        setHighlightLinks(highlightLinks);
+    };
 
-    // const handleNodeHover = (node) => {
-    //     highlightNodes.clear();
-    //     highlightLinks.clear();
-    //     if (node) {
-    //         highlightNodes.add(node);
-    //         node.neighbors.forEach((neighbor) => highlightNodes.add(neighbor));
-    //         node.links.forEach((link) => highlightLinks.add(link));
-    //     }
+    const handleNodeHover = (node) => {
+        highlightNodes.clear();
+        highlightLinks.clear();
+        if (node) {
+            highlightNodes.add(node);
+            node.neighbors.forEach((neighbor) => highlightNodes.add(neighbor));
+            node.links.forEach((link) => highlightLinks.add(link));
+        }
 
-    //     setHoverNode(node || null);
-    //     updateHighlight();
-    // };
+        setHoverNode(node || null);
+        updateHighlight();
+    };
 
-    // const handleLinkHover = (link) => {
-    //     highlightNodes.clear();
-    //     highlightLinks.clear();
+    const handleLinkHover = (link) => {
+        highlightNodes.clear();
+        highlightLinks.clear();
 
-    //     if (link) {
-    //         highlightLinks.add(link);
-    //         highlightNodes.add(link.source);
-    //         highlightNodes.add(link.target);
-    //     }
+        if (link) {
+            highlightLinks.add(link);
+            highlightNodes.add(link.source);
+            highlightNodes.add(link.target);
+        }
 
-    //     updateHighlight();
-    // };
+        updateHighlight();
+    };
 
-    // const paintRing = useCallback(
-    //     (node, ctx) => {
-    //         // add ring just for highlighted nodes
-    //         ctx.beginPath();
-    //         ctx.arc(node.x, node.y, NODE_R * 1.4, 0, 2 * Math.PI, false);
-    //         ctx.fillStyle = node === hoverNode ? "red" : "orange";
-    //         ctx.fill();
-    //     },
-    //     [hoverNode]
-    // );
+    const paintRing = useCallback(
+        (node, ctx) => {
+            // add ring just for highlighted nodes
+            ctx.beginPath();
+            ctx.arc(node.x, node.y, NODE_R * 1.4, 0, 2 * Math.PI, false);
+            ctx.fillStyle = node === hoverNode ? "red" : "orange";
+            ctx.fill();
+        },
+        [hoverNode]
+    );
 
     return (
         <ForceGraph3D
@@ -64,28 +64,28 @@ const Graph = () => {
             // nodeRelSize={NODE_R}
             nodeColor="white"
             nodeId="id"
-            nodeLabel="id"
+            nodeLabel="name"
             linkCurvature={0.1}
             nodeThreeObject={({id}) => {
                 const imgTexture = new TextureLoader().load(`../../../media/chains/${id}.png`);
                 const material = new SpriteMaterial({ map: imgTexture });
                 const sprite = new Sprite(material);
-                sprite.scale.set(12, 12);
+                sprite.scale.set(20, 20);
       
                 return sprite;
             }}
-            // autoPauseRedraw={false}
-            // linkWidth={(link) => (highlightLinks.has(link) ? 5 : 1)}
-            // linkDirectionalParticles={4}
-            // linkDirectionalParticleWidth={(link) =>
-            //     highlightLinks.has(link) ? 4 : 0
-            // }
-            // nodeCanvasObjectMode={(node) =>
-            //     highlightNodes.has(node) ? "before" : undefined
-            // }
-            // nodeCanvasObject={paintRing}
-            // onNodeHover={handleNodeHover}
-            // onLinkHover={handleLinkHover}
+            autoPauseRedraw={false}
+            linkWidth={(link) => (highlightLinks.has(link) ? 5 : 1)}
+            linkDirectionalParticles={4}
+            linkDirectionalParticleWidth={(link) =>
+                highlightLinks.has(link) ? 4 : 0
+            }
+            nodeCanvasObjectMode={(node) =>
+                highlightNodes.has(node) ? "before" : undefined
+            }
+            nodeCanvasObject={paintRing}
+            onNodeHover={handleNodeHover}
+            onLinkHover={handleLinkHover}
         />
     );
 };
