ba62d16063be36edc6185d921d7d7c98a0d0c347
Master Pi <minh.vln140501@gmail.com>
2022-07-08 11:35:07 +0700

    graph

--- src/Body/Infrastructure/components/Graph.js
+++ src/Body/Infrastructure/components/Graph.js
@@ -1,63 +1,77 @@
-import React, { Component } from "react";
+import React from "react";
 import ForceGraph3D from "react-force-graph-3d";
 import "./Graph.css";
-import { Sprite, CanvasTexture, SpriteMaterial } from "three";
+import Data from "./relayers.json";
+import { useState, useCallback } from "react";
 
 const Graph = () => {
-  const genRandomTree = (N = 50) => {
-    return {
-      nodes: [...Array(N).keys()].map(i => ({
-        id: i,
-        canvas: drawCircle()
-      })),
-      links: [...Array(N).keys()]
-        .filter(id => id)
-        .map(id => ({
-          source: id,
-          target: Math.round(Math.random() * (id - 1))
-        }))
-    };
+  const NODE_R = 8;
+  const [highlightNodes, setHighlightNodes] = useState(new Set());
+  const [highlightLinks, setHighlightLinks] = useState(new Set());
+  const [hoverNode, setHoverNode] = useState(null);
 
-    //draw a circle to a canvas and return
-    function drawCircle() {
-      let canvas = document.createElement("canvas");
-      canvas.id = "canvas";
-      canvas.width = 32;
-      canvas.height = 32;
-      let ctx = canvas.getContext("2d");
-      let PI2 = Math.PI * 2;
-      ctx.arc(16, 16, 16, 0, PI2, true);
-      ctx.fillStyle = "red";
-      ctx.fill();
-
-      return canvas;
-    }
+  const updateHighlight = () => {
+    setHighlightNodes(highlightNodes);
+    setHighlightLinks(highlightLinks);
   };
 
-    const data = genRandomTree();
-    return (
-      <ForceGraph3D
-        className="infra-graph"
-        graphData={data}
-        backgroundColor="grey"
-        nodeLabel="id"
-        width={2000}
-        height={2000}
-        nodeId="id"
-        linkCurvature={0.1}
-        nodeThreeObject={({ canvas }) => {
-          const imageTexture = new CanvasTexture(canvas);
-          // imageTexture.needsUpdate = true;
-          const material = new SpriteMaterial({
-            map: imageTexture,
-            transparent: false
-          });
-          const sprite = new Sprite(material);
-          sprite.scale.set(16, 16);
-          return sprite;
-        }}
-      />
-    );
+  const handleNodeHover = node => {
+    highlightNodes.clear();
+    highlightLinks.clear();
+    if (node) {
+      highlightNodes.add(node);
+      node.neighbors.forEach(neighbor => highlightNodes.add(neighbor));
+      node.links.forEach(link => highlightLinks.add(link));
+    }
+
+    setHoverNode(node || null);
+    updateHighlight();
+  };
+
+  const handleLinkHover = link => {
+    highlightNodes.clear();
+    highlightLinks.clear();
+
+    if (link) {
+      highlightLinks.add(link);
+      highlightNodes.add(link.source);
+      highlightNodes.add(link.target);
+    }
+
+    updateHighlight();
+  };
+
+  const paintRing = useCallback((node, ctx) => {
+    // add ring just for highlighted nodes
+    ctx.beginPath();
+    ctx.arc(node.x, node.y, NODE_R * 1.4, 0, 2 * Math.PI, false);
+    ctx.fillStyle = node === hoverNode ? 'red' : 'orange';
+    ctx.fill();
+  }, [hoverNode]);
+
+  return (
+    <ForceGraph3D
+      className="infra-graph"
+      graphData={Data}
+      // backgroundColor="rgba(0,0,0,0)"
+      backgroundColor="grey"
+      nodeLabel="id"
+      width={1000}
+      nodeColor="white"
+      height={1000}
+      nodeId="id"
+      linkCurvature={0.1}
+      nodeRelSize={NODE_R}
+      autoPauseRedraw={false}
+      linkWidth={link => highlightLinks.has(link) ? 5 : 1}
+      linkDirectionalParticles={4}
+      linkDirectionalParticleWidth={link => highlightLinks.has(link) ? 4 : 0}
+      nodeCanvasObjectMode={node => highlightNodes.has(node) ? 'before' : undefined}
+      nodeCanvasObject={paintRing}
+      onNodeHover={handleNodeHover}
+      onLinkHover={handleLinkHover}
+    />
+  );
 }
 
 export default Graph;
