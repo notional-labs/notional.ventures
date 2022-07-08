d894ceb6714c4a6d15b6d8c404c2c94063df29a9
Du Nguyen <61083705+lichdu29@users.noreply.github.com>
2022-07-08 16:11:00 +0700

    remove anone

--- src/Body/Project/Project.js
+++ src/Body/Project/Project.js
@@ -1,18 +1,11 @@
 import React from "react";
 import "./Project.css";
 import Diglogo from "../../media/logo/dig-logo.png";
-import Anonelogo from "../../media/logo/another-logo.png";
 import Craft from "../../media/logo/economy-logo.png";
 import ProjectList from "./ProjectList";
 
 const Project = () => {
   const PROJECTS = [
-    {
-      id: "p1",
-      image: `${Anonelogo}`,
-      name: "Another-1",
-      url: "https://anone.notional.ventures"
-    },
     {
       id: "p2",
       image: `${Diglogo}`,
