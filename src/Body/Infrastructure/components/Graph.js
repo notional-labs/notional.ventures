5eda557af371306718c0c305d9d6f432b8e0c89e
Du Nguyen <dunguyen643@gmail.com>
2022-08-07 01:46:07 +0700

    progress on graph

--- src/Body/Infrastructure/components/Graph.js
+++ src/Body/Infrastructure/components/Graph.js
@@ -1,93 +1,109 @@
+// import React from "react";
+// import ForceGraph3D from "react-force-graph-3d";
+// import loadable from '@loadable/component'
+// // import SpriteText from "three-spritetext";
+// // import THREE from "react-three"
+// // import  from "three-spritetext"
+// import * as THREE from 'three';
+// import data from './test-img.json'
+// const Graph = () => {
+//     const img = data.nodes.map((id,img) => img)
+//     let ForceGraph3D = loadable(() => import(`./members/${img}`));
+//     const nodeToThree = ({img}) => {
+//         // console.log({img});
+//         const imgTexture = new THREE.TextureLoader().load(`./members/${img}`);
+//         // console.log(imgTexture);
+//         const material = new THREE.SpriteMaterial({ map: imgTexture });
+//         // console.log(material);
+//         const sprite = new THREE.Sprite(material);
+//         // console.log(sprite);
+//         sprite.scale.set(12, 12);
+//         return sprite;
+//     }
+//     return (
+//         <ForceGraph3D
+//             graphData={data}
+//             nodeThreeObject={nodeToThree}
+//         />
+//     )
+// }
+
+// export default Graph;
+
+// ({img}) => {
+//     console.log({img});
+//     const imgTexture = new THREE.TextureLoader().load(`./members/${img}`);
+//     console.log(imgTexture);
+//     const material = new THREE.SpriteMaterial({ map: imgTexture });
+//     console.log(material);
+//     const sprite = new THREE.Sprite(material);
+//     console.log(sprite);
+//     sprite.scale.set(12, 12);
+//     return sprite;
+// }
+
 import React from "react";
+// import renderMindMap from "./renderMindMap";
 import ForceGraph3D from "react-force-graph-3d";
-import "./Graph.css";
-import {Data} from "./relayers";
-// import { Sprite, TextureLoader, SpriteMaterial } from "three";
-import { useState, useCallback } from "react";
+import * as THREE from "three";
 
-const Graph = () => {
-    
-    const gData = Data;
-    console.log(Data);
+// import MindMap from "./MindMap";
 
-    gData.links.forEach(link => {
-        let a = gData.nodes[link.source];
-        console.log(a)
-        const b = gData.nodes[link.target];
-        // gData.nodes[a]
-        !a.neighbors && (a.neighbors = []);
-        !b.neighbors && (b.neighbors = []);
-        a.neighbors.push(b);
-        b.neighbors.push(a);
-  
-        !a.links && (a.links = []);
-        !b.links && (b.links = []);
-        a.links.push(link);
-        b.links.push(link);
-      });
-      
 
-      const highlightNodes = new Set();
-      const highlightLinks = new Set();
-      let hoverNode = null;
-      
-    const updateHighlight = () => {
-        // Graph
-    };
-
-    return (
-        <ForceGraph3D
-            graphData={gData}
-            backgroundColor="rgba(10,10,10,100)"
-            height={700}
-            width={1000}
-            nodeColor={node => 
-                highlightNodes.has(node) ? node === hoverNode ? 'rgb(255,0,0,1)' : 'rgba(255,160,0,0.8)' : 'rgba(0,255,255,0.6)'
-            }
-            nodeId="id"
-            nodeLabel="name"
-            linkCurvature={0.1}
-            // nodeThreeObject={({id}) => {
-            //     const imgTexture = new TextureLoader().load(`../../../media/chains/${id}.png`);
-            //     const material = new SpriteMaterial({ map: imgTexture });
-            //     const sprite = new Sprite(material);
-            //     sprite.scale.set(20, 20);
-      
-            //     return sprite;
-            // }}
-            linkWidth={(link) => (highlightLinks.has(link) ? 4 : 1)}
-            linkDirectionalParticles={link => highlightLinks.has(link) ? 4 : 0}
-            linkDirectionalParticleWidth={4}
-            onNodeHover={node => {
-                // no state change
-                if ((!node && !highlightNodes.size) || (node && hoverNode === node)) return;
-
-                highlightNodes.clear();
-                highlightLinks.clear();
-                if (node) {
-                    highlightNodes.add(node);
-                    node.neighbors.forEach(neighbor => highlightNodes.add(neighbor));
-                    node.links.forEach(link => highlightLinks.add(link));
-                }
-
-                hoverNode = node || null;
-
-                updateHighlight();
-            }}
-            onLinkHover={link => {
-                highlightNodes.clear();
-                highlightLinks.clear();
-      
-                if (link) {
-                  highlightLinks.add(link);
-                  highlightNodes.add(link.source);
-                  highlightNodes.add(link.target);
-                }
-      
-                updateHighlight();
-            }}
-        />
-    );
+// Random connected graph
+const gData = {
+    "nodes": [
+        {"id": "billy", "img": "billy.jpg"},
+        {"id": "chinh", "img": "chinh.jpg"},
+        {"id": "du1", "img": "du1.jpg"},
+        {"id": "Hieu1", "img": "Hieu1.jpg"},
+        {"id": "Huy1", "img": "Huy1.jpg"},
+        {"id": "khanh1", "img": "khanh1.jpg"},
+        {"id": "long", "img": "long.jpg"},
+        {"id": "minh", "img": "minh.jpg"},
+        {"id": "son", "img": "son.jpg"},
+        {"id": "vinh1", "img": "vinh1.jpg"},
+        {"id": "vuong1", "img": "vuong.jpg"}
+      ],
+      "links": [
+        {"source": "billy", "target": "son"},
+        {"source": "long", "target": "son"},
+        {"source": "vuong1", "target": "son"},
+        {"source": "vuong1", "target": "long"},
+        {"source": "minh", "target": "son"},
+        {"source": "khanh1", "target": "son"},
+        {"source": "du1", "target": "son"},
+        {"source": "Huy1", "target": "son"},
+        {"source": "chinh", "target": "son"},
+        {"source": "chinh", "target": "son"},
+        {"source": "Hieu1", "target": "du1"},
+        {"source": "Hieu1", "target": "vuong1"},
+        {"source": "Hieu1", "target": "long"},
+        {"source": "Hieu1", "target": "son"},
+        {"source": "minh", "target": "Hieu1"},
+        {"source": "du1", "target": "Hieu1"},
+        {"source": "chinh", "target": "Hieu1"},
+        {"source": "long", "target": "Hieu1"},
+        {"source": "long", "target": "chinh"},
+        {"source": "long", "target": "chinh"},
+        {"source": "vinh1", "target": "chinh"},
+        {"source": "son", "target": "khanh1"},
+        {"source": "son", "target": "long"}
+      ]
 };
 
-export default Graph;
+export default function App() {
+  return (
+    <ForceGraph3D
+      graphData={gData}
+      nodeThreeObject={({ img }) => {
+        const imgTexture = new THREE.TextureLoader().load(`./members/${img}`);
+        const material = new THREE.SpriteMaterial({ map: imgTexture });
+        const sprite = new THREE.Sprite(material);
+        sprite.scale.set(12, 12);
+
+        return sprite;
+      }}
+    />
+  );
+}
