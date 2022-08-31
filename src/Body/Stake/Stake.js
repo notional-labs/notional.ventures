ef5556b12e394b6e2fb951e49fdaf2ab0cd8fe6f
Du Nguyen <dunguyen643@gmail.com>
2022-08-31 23:59:47 +0700

    responsive for new ui #1

--- src/Body/Stake/Stake.js
+++ src/Body/Stake/Stake.js
@@ -4,24 +4,24 @@ import { STAKES } from "./chains-data";
 import StakeList from "./StakeList";
 
 const Stake = () => {
-    return (
-        <div id="Delegate" className="stake-section">
-            <div className="title">
-                <hr className="bar"></hr>
-                <p className="name">Stake With Us</p>
-                <p className="text">
-                    Notional uses in-house hardware and has developers in the
-                    office 24/7 on shifts to quickly respond to issues as they
-                    arise. Validating outside of the cloud increases
-                    decentralization and dramatically improves epoch time,
-                    meaning we miss less blocks, and you get better rewards.{" "}
-                </p>
-            </div>
-            <div className="stake">
-                <StakeList stakes={STAKES} />
-            </div>
-        </div>
-    );
+  return (
+    <div id="Delegate" className="stake-section">
+      <div className="title">
+        <hr className="bar"></hr>
+        <p className="name">Stake With Us</p>
+        <p className="text">
+          Notional uses in-house hardware and has developers in the office 24/7
+          on shifts to quickly respond to issues as they arise. Validating
+          outside of the cloud increases decentralization and dramatically
+          improves epoch time, meaning we miss less blocks, and you get better
+          rewards.{" "}
+        </p>
+      </div>
+      <div className="stake">
+        <StakeList stakes={STAKES} />
+      </div>
+    </div>
+  );
 };
 
 export default Stake;
