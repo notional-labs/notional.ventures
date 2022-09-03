1873deb9ff86a48aadfa6def8302f8f277b72404
Master Pi <minh.vln140501@gmail.com>
2022-09-04 00:42:05 +0700

    fix path

--- src/Body/Infrastructure/components/SnapshotModal.js
+++ src/Body/Infrastructure/components/SnapshotModal.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 import { CSSTransition } from "react-transition-group";
 import Backdrop from "../../Stake/Backdrop";
 import "./SnapshotModal.css";
-import Modal_divisor from "../../media/stake/modal-divisor.png";
-import Info_divisor from "../../media/stake/info-divisor.png";
+import Modal_divisor from "../../../media/stake/modal-divisor.png";
+import Info_divisor from "../../../media/stake/info-divisor.png";
 
 const ModalOverlay = (props) => {
   const [pebbledb, setPebbledb] = useState(false);
