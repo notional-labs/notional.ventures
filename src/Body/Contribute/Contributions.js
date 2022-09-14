7760e4d6189233b41c956071d94a5d15c89fa846
Master Pi <minh.vln140501@gmail.com>
2022-09-15 01:25:04 +0700

    add validator value

--- src/Body/Contribute/Contributions.js
+++ src/Body/Contribute/Contributions.js
@@ -1,63 +1,79 @@
 import "./Contributions.css";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import AutoSlide from "./PartnerCarousel";
 import Filter from "../../media/imgs/Contribution/filter-contribution.png";
+import axios from "axios";
 import Abstract from "../../media/imgs/Contribution/abstract-contribution.png";
 
 const Contributions = () => {
-    return (
-        <div className="contributions">
-            <img src={Filter} className="filter" />
-            <div id="Contribution" className="title">
-                <hr className="bar"></hr>
-                <p className="name">Contributions</p>
-                <p className="text">
-                    Notional Labs is committed to engaging in governance and
-                    community matters on every chain we validate. A team member
-                    is delegated to each chain to ensure dependable, genuine
-                    communication with project stakeholders. We also commit code
-                    to every chain we validate and have members on the core
-                    teams of several major projects, including Osmosis, Juno,
-                    and Sifchain.{" "}
-                </p>
-            </div>
-            <img src={Abstract} className="abstract-contribution" />
-            <AutoSlide />
+  const [data, setData] = useState([]);
 
-            <div className="companyinfo">
-                <div className="companyinfo-item companyinfo-item__1">
-                    <div className="infoStyle">
-                        <div className="info-number">28</div>
-                        <div className="info-text">Chains operating</div>
-                    </div>
-                </div>
-                <div className="companyinfo-item companyinfo-item__2">
-                    <div className="infoStyle">
-                        <div className="info-number">99,3%</div>
-                        <div className="info-text">Uptime</div>
-                    </div>
-                </div>
-                <div className="companyinfo-item companyinfo-item__3">
-                    <div className="infoStyle">
-                        <div className="info-number">2K+</div>
-                        <div className="info-text">Unique Users</div>
-                    </div>
-                </div>
-                <div className="companyinfo-item companyinfo-item__4">
-                    <div className="infoStyle">
-                        <div className="info-number">$12M</div>
-                        <div className="info-text">Company Value</div>
-                    </div>
-                </div>
-                <div className="companyinfo-item companyinfo-item__5">
-                    <div className="infoStyle">
-                        <div className="info-number">~$200K</div>
-                        <div className="info-text">Annual Rewards</div>
-                    </div>
-                </div>
-            </div>
+  useEffect(() => {
+    fetchData();
+  }, []);
+   
+  const fetchData = async () => {
+    try {
+      const res = await axios.get(`https://backend.notional.ventures`);
+      setData(res.data);
+      console.log(data); 
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+  return (
+    <div className="contributions">
+      <img src={Filter} className="filter" />
+      <div id="Contribution" className="title">
+        <hr className="bar"></hr>
+        <p className="name">Contributions</p>
+        <p className="text">
+          Notional Labs is committed to engaging in governance and community
+          matters on every chain we validate. A team member is delegated to each
+          chain to ensure dependable, genuine communication with project
+          stakeholders. We also commit code to every chain we validate and have
+          members on the core teams of several major projects, including
+          Osmosis, Juno, and Sifchain.{" "}
+        </p>
+      </div>
+      <img src={Abstract} className="abstract-contribution" />
+      <AutoSlide />
+
+      <div className="companyinfo">
+        <div className="companyinfo-item companyinfo-item__1">
+          <div className="infoStyle">
+            <div className="info-number">{data.chains}</div>
+            <div className="info-text">Chains operating</div>
+          </div>
         </div>
-    );
+        <div className="companyinfo-item companyinfo-item__2">
+          <div className="infoStyle">
+            <div className="info-number">{data.uptime * 100}%</div>
+            <div className="info-text">Uptime</div>
+          </div>
+        </div>
+        <div className="companyinfo-item companyinfo-item__3">
+          <div className="infoStyle">
+            <div className="info-number">{data.validatorsValue}</div>
+            <div className="info-text">Validator's Value</div>
+          </div>
+        </div>
+        <div className="companyinfo-item companyinfo-item__4">
+          <div className="infoStyle">
+            <div className="info-number">{data.companyValue}</div>
+            <div className="info-text">Company Value</div>
+          </div>
+        </div>
+        <div className="companyinfo-item companyinfo-item__5">
+          <div className="infoStyle">
+            <div className="info-number">{data.annualReward}</div>
+            <div className="info-text">Annual Rewards</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contributions;
