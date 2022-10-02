97ac1ad871356dfff17c614bbac4078de908dfe0
Master Pi <minh.vln140501@gmail.com>
2022-10-03 00:51:12 +0700

    change server

--- src/Header/Navbar.js
+++ src/Header/Navbar.js
@@ -39,9 +39,9 @@ const Navbar = () => {
             <li className="dropdown-menu">
               <a href="#Infra">Infrastructure</a>
               <div className="navigation-content">
-                <Link to="/endpoint">Endpoints</Link>
-                <Link to="/snapshot">Snapshots</Link>
-                <Link to="/upgrade">Chains Upgrade</Link>
+                <Link to="/endpoint" style={{fontSize:"20px"}}>Endpoints</Link>
+                <Link to="/snapshot" style={{fontSize:"20px"}}>Snapshots</Link>
+                <Link to="/upgrade" style={{fontSize:"20px"}}>Chains Upgrade</Link>
               </div>
             </li>
             <li>
@@ -92,9 +92,9 @@ const Navbar = () => {
             <li className="dropdown-menu">
               <a href="#Infra">Infrastructure</a>
               <div className="navigation-content">
-                <Link to="/endpoint">Endpoints</Link>
-                <Link to="/snapshot">Snapshots</Link>
-                <Link to="/upgrade">Chains Upgrade</Link>
+                <Link to="/endpoint" style={{fontSize:"20px"}}>Endpoints</Link>
+                <Link to="/snapshot" style={{fontSize:"20px"}}>Snapshots</Link>
+                <Link to="/upgrade" style={{fontSize:"20px"}}>Chains Upgrade</Link>
               </div>
             </li>
             <li>
