919c455c10b206d34bd6e45e524bea79ce8e49ba
Du Nguyen <dunguyen643@gmail.com>
2022-10-05 22:07:25 +0700

    fix conflict

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Project from "./Body/Project/Project";
 import Team from "./Body/Team/Team";
 import Contact from "./Body/Contact/Contact";
 import Footer from "./Footer/Footer";
-import GifLoader from "react-gif-loader";
 import { useState, useEffect } from "react";
 import Loading from "./media/imgs/loading.webm";
 import { useParams } from "react-router-dom";
@@ -18,59 +17,51 @@ const facts = [
   "We are changing our Endpoint services, please go to Endpoints section to see the new endpoints",
 ];
 
-
 const Home = (props) => {
-    return (
-        <>
-            <Header />
-            <Contributions />
-            <Stake 
-            showModal = {props.showModal}
-            id = {props.id}
-            />
-            <Infrastructure />
-            <Project />
-            <Team />
-            <Contact />
-            <Footer />
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <Contributions />
+      <Stake showModal={props.showModal} id={props.id} />
+      <Infrastructure />
+      <Project />
+      <Team />
+      <Contact />
+      <Footer />
+    </>
+  );
 };
 
 function App(props) {
-    let {id} = useParams();
-    const [isLoading, setIsLoading] = useState(false);
-    const randomText = facts[Math.floor(Math.random() * facts.length)];
-    useEffect (() => {
-      setIsLoading(true)
-      setTimeout(() => {
-        setIsLoading(false)
-      }, 4000)
-    },[])
-    return (
-        <React.Fragment>
-            {isLoading && (
-                <div className="loader-container">
-                    <GifLoader 
-                        className="loading-logo"
-                        loading={true}
-                        imageSrc={Loading}
-                    />
-                    <p className="loading-title">DID YOU KNOW</p>
-                    <p className="loading-text">{randomText}</p>
-                </div>
-            )}
-            {!isLoading && (
-                <div className="root">
-                    <Home 
-                        showModal = {props.showModal}
-                        id = {id}
-                    />
-                </div>
-            )}
-        </React.Fragment>
-    );
-
+  let { id } = useParams();
+  const [isLoading, setIsLoading] = useState(false);
+  const randomText = facts[Math.floor(Math.random() * facts.length)];
+  useEffect(() => {
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 4000);
+  }, []);
+  return (
+    <React.Fragment>
+      {isLoading && (
+        <div className="loader-container">
+          <div className="loader-container">
+            <video autoPlay loop muted playsInline>
+              <source src={Loading} type="video/webm" />
+            </video>
+            <p className="loading-title">DID YOU KNOW</p>
+            <p className="loading-text">{randomText}</p>
+          </div>
+        </div>
+      )}
+      {!isLoading && (
+        <div className="root">
+          <Home showModal={props.showModal} id={id} />
+        </div>
+      )}
+    </React.Fragment>
+  );
 }
 
 export default App;
