52fd077ba99f193e59c180aa742ff1244818d6b5
Master Pi <minh.vln140501@gmail.com>
2022-10-06 16:09:00 +0700

    ahihi

--- src/Body/Infrastructure/components/Upgrade/ChainUpgradeItem.js
+++ src/Body/Infrastructure/components/Upgrade/ChainUpgradeItem.js
@@ -8,7 +8,7 @@ const ChainUpgradeItem = (props) => {
         <td
           onClick={() =>
             window.open(
-              `https://explorer.notional.ventures/${props.ping}/gov/${props.id}`
+              `https://ping.pub/${props.ping}/gov/${props.id}`
             )
           }
           style={{ cursor: "pointer" }}
